7a3a56c54ff19b24832f63fd2a6cbf87d1768ab3
최준영 <168086573+SKyhumn@users.noreply.github.com>
2025-01-20 00:02:11 +0900

    Create 원소들의 곱과 합 lv.0.js

---
+++ lv.0/원소들의 곱과 합 lv.0.js
@@ -0,0 +1,16 @@
+function solution(num_list) {
+    var answer = 0;
+    let a=1,b=0;
+    for(let i=0;i<num_list.length;i++){
+        a*=num_list[i];
+        b+=num_list[i];
+    }
+    b=b**2;
+    if(a<b){
+        answer=1;
+    }
+    else{
+        answer=0;
+    }
+    return answer;
+}
