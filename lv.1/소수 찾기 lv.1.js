a93412d1056f265e9cff3fa87ac8837d58556f06
최준영 <168086573+SKyhumn@users.noreply.github.com>
2025-03-27 21:01:51 +0900

    Update 소수 찾기 lv.1.js

--- lv.1/소수 찾기 lv.1.js
+++ lv.1/소수 찾기 lv.1.js
@@ -1,16 +1,14 @@
+function isPrime(x) {
+  for (let i = 2; i <= Math.sqrt(x); i++) {
+    if (x % i === 0) return false;
+  }
+  return true;
+}
+
 function solution(n) {
-    var answer = 0;
-    let count=0;
-    for(let i=1;i<=n;i++){
-        for(let j=1;j<=i;j++){
-            if(i%j==0){
-                count++;
-            }
-        }
-        if(count==2){
-            answer++
-        }
-        count=0;
-    }
-    return answer;
+  let answer = 0;
+  for (let i = 2; i <= n; i++) {
+    if (isPrime(i)) answer++;
+  }
+  return answer;
 }
