61d25e800f45f85434ba10d44784298af573a37f
최준영 <168086573+SKyhumn@users.noreply.github.com>
2025-09-18 19:50:55 +0900

    Update 없는 숫자 더하기 lv.1.js

--- lv.1/없는 숫자 더하기 lv.1.js
+++ lv.1/없는 숫자 더하기 lv.1.js
@@ -1,9 +1,4 @@
 function solution(numbers) {
-    let answer=0;
-    const arr=[0,1,2,3,4,5,6,7,8,9];
-    let other=arr.filter(item=>!numbers.includes(item));
-    for(let i=0;i<other.length;i++){
-        answer+=other[i];
-    }
+    let answer=45-numbers.reduce((acc,curr)=>acc+curr,0);
     return answer;
 }
